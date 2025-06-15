227634c7568175e76a13d14b2527db848458192c
NurHary <theliq310@gmail.com>
2025-06-15 18:20:56 +0700

    sukses halo dunia

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,83 @@
-
-use eframe::egui;
-
-struct MyApp{
-    name: String,
-    age: u32;
+enum TimeMode {
+    Play,
+    Pause,
+    Stop,
 }
 
-impl Default for MyApp{
+enum Model {
+    Pomodoro,
+    Stopwatch,
+    Timer,
+}
 
+struct SlotModel {
+    model: Model,
+    duration: u32,
+}
+
+impl SlotModel {
+    fn new(model: Model, duration: u32) -> Self {
+        Self { model, duration }
+    }
+}
+
+impl Default for SlotModel {
+    fn default() -> Self {
+        Self {
+            model: Model::Pomodoro,
+            duration: 1500,
+        }
+    }
+}
+
+struct TimeSlot {
+    mode: TimeMode,
+    model: SlotModel,
+}
+
+impl Default for TimeSlot {
+    fn default() -> Self {
+        Self {
+            mode: TimeMode::Stop,
+            model: SlotModel::default(),
+        }
+    }
+}
+
+struct MyApp {
+    slot: Vec<TimeSlot>,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            slot: vec![TimeSlot::default(), TimeSlot::default()],
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
+            ui.heading("Halo Dunia");
+            ui.heading(format!("Waktu anda {}", self.slot[0].model.duration));
+        });
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let native_opt = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_always_on_top()
+            .with_inner_size([350.0, 100.0]),
+        ..Default::default()
+    };
+    let _ = eframe::run_native(
+        "Halo Dunia",
+        native_opt,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Ok(Box::<MyApp>::default())
+        }),
+    );
 }
